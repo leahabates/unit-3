23fe09168c2b2580b3e3072fa665280200668f22
leahabates <lbates3@wisc.edu>
2025-03-17 13:22:51 -0400

    lab 2 mod 2 lesson 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 //insert code here!
-var caliCounty = 
+window.onload = setMap;
+
+//set up choropleth map
+function setMap(){
+	//using promise.all() to parallelize asynchronous data loading
+	var promises = [
+		d3.csv ("data/calihealth.csv"),
+		d3.json("data/calicounty.topojson")
+	];
+	Promise.all(promises).then(callback);
+
+	function callback(data){
+		var csvData = data[0];
+		var	california = data[1];
+		console.log(csvData);
+		console.log(california);
+
+		var californiaCounties = topojson.feature(california, california.objects.calicounty);
+
+	//examine results
+		console.log(californiaCounties);
+
+	}
+};
